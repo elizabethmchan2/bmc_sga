d28bab62d30aababcac757390d21d4a5ec6f102e
elizabethmchan2 <elizabethmchan2@gmail.com>
2018-04-30 10:19:26 -0400

    changed jquery in repco.js to pure javascript

--- js/repco.js
+++ js/repco.js
@@ -1,21 +1,85 @@
-$(document).ready(function () {
+//$(document).ready(function () {
+//
+//    $("#classpresidentsstuff").hide();
+//    $("#electionstuff").hide();
+//    $("#membersstuff").hide();
+//    $("#dormpresidentsstuff").hide();
+//
+//    $("#classpresidents").click(function () {
+//        $("#classpresidentsstuff").toggle();
+//    });
+//
+//    $("#election").click(function () {
+//        $("#electionstuff").toggle();
+//    });
+//    $("#members").click(function () {
+//        $("#membersstuff").toggle();
+//    });
+//    $("#dormpresidents").click(function () {
+//        $("#dormpresidentsstuff").toggle();
+//    });
+//});
 
-    $("#classpresidentsstuff").hide();
-    $("#electionstuff").hide();
-    $("#membersstuff").hide();
-    $("#dormpresidentsstuff").hide();
+var classpresidents = document.getElementById("classpresidents")
+var classpresidentsstuff = document.getElementById("classpresidentsstuff")
 
-    $("#classpresidents").click(function () {
-        $("#classpresidentsstuff").toggle();
-    });
+var elections = document.getElementById("elections")
+var electionsstuff = document.getElementById("electionsstuff")
 
-    $("#election").click(function () {
-        $("#electionstuff").toggle();
-    });
-    $("#members").click(function () {
-        $("#membersstuff").toggle();
-    });
-    $("#dormpresidents").click(function () {
-        $("#dormpresidentsstuff").toggle();
-    });
+var members = document.getElementById("members")
+var membersstuff = document.getElementById("membersstuff")
+
+var dormpresidents = document.getElementById("dormpresidents")
+var dormpresidentsstuff = document.getElementById("dormpresidentsstuff")
+
+
+var classReset = 0;
+
+classpresidents.addEventListener('click', function() {
+    if (classReset == 0) { //not yet clicked
+        classpresidentsstuff.style.display = "block";
+        classReset = 1;
+
+    } else {
+        classpresidentsstuff.style.display = "none";
+        classReset = 0;
+    }
+});
+
+var electionReset = 0;
+elections.addEventListener('click', function() {
+    if (electionReset == 0) { //not yet clicked
+        electionsstuff.style.display = "block";
+        electionReset = 1;
+//
+    } else {
+        electionsstuff.style.display = "none";
+        electionReset = 0;
+    }
+});
+
+
+var memberReset = 0;
+members.addEventListener('click', function() {
+    if (memberReset == 0) { //not yet clicked
+        membersstuff.style.display = "block";
+        memberReset = 1;
+//
+    } else {
+        membersstuff.style.display = "none";
+        memberReset = 0;
+    }
+});
+
+
+var dormReset = 0;
+dormpresidents.addEventListener('click', function() {
+    if (dormReset == 0) { //not yet clicked
+        dormpresidentsstuff.style.display = "block";
+        dormReset = 1;
+//
+    } else {
+        dormpresidentsstuff.style.display = "none";
+        dormReset = 0;
+    }
 });
